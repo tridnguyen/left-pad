5786cef852206282236a57a30d8647b31dffc8a4
Tri Nguyen <nguyentrid@gmail.com>
2016-04-01 13:36:37 -0700

    Initial Commit

---
+++ left-pad.js
@@ -0,0 +1,17 @@
+if (typeof String.prototype.leftPad !== 'funnction') {
+	String.prototype.leftPad = function (len, padString) {
+    var delta;
+    len = parseInt(len, 10);
+    delta = len - this.length;
+
+    if (delta < 0) {
+      return this;
+    }
+
+    if (!padString && padString !== 0) {
+      padString = ' ';
+    }
+
+    return new Array(delta+1).join(padString).substr(0, delta) + this;
+  }
+}
---
+++ spec/left-pad_spec.js
@@ -0,0 +1,34 @@
+'use strict';
+describe('leftPad', function () {
+  describe('when string to pad is same length as length to pad', function () {
+    it('left pads the string with 0', function () {
+      expect('donothing'.leftPad(9, 0)).toEqual('donothing');
+    });
+  });
+
+  describe('when string to pad’s length is greater than length to pad', function () {
+    it('left pads the string with 0', function () {
+      expect('already long enough string'.leftPad(9, 0)).toEqual('already long enough string');
+    });
+  });
+
+  describe('when a string to pad’s length is less than the length to pad', function () {
+    describe('when string to pad is 0', function () {
+      it('left pads the string with 0', function () {
+        expect('test'.leftPad(9, 0)).toEqual('00000test');
+      });
+    });
+
+    describe('when string to pad is not provided', function () {
+      it('left pads the string with an empty string', function () {
+        expect('test'.leftPad(9)).toEqual('     test');
+      });
+    });
+
+    describe('when value to pad is more than one character in length', function () {
+      it('left pads the string with a repeated value until it reaches the desired length', function () {
+        expect('test'.leftPad(30, 'multiple_chars_')).toEqual('multiple_chars_multiple_chtest');
+      });
+    });
+  });
+});
